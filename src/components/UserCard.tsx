8953f44fa11f885e450dfc40bf3bc4878d0ae57b
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-12-04 12:12:45 -0300

    feat: add user page

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,23 +1,47 @@
-import { Avatar, Card, Heading, Stack, Text } from '@chakra-ui/react'
+import { Avatar, Button, Card, Heading, Stack, Text } from '@chakra-ui/react'
 import { IUser } from '../types/types'
+import { Tools } from './Tools'
+import { Trash } from '@phosphor-icons/react'
+import { Dialog } from './Dialog'
 
-export const UserCard = ({ email, id, name, role }: IUser) => {
+interface UserCardProps extends IUser {
+  onDelete?: (id: number) => void
+}
+
+export const UserCard = ({ email, id, name, role, onDelete }: UserCardProps) => {
   const userRole = role === 'ADMIN' ? 'Administrador' : 'Usuário'
 
   return (
-    <Card w="150px" p={4} gap={2}>
-      <Stack>
-        <Avatar name={name} alignSelf="center" />
-        <Heading size="sm" maxW="100%" overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis">{name}</Heading>
-      </Stack>
-      <Stack maxW="120px">
-        <Text maxW="100%" overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis">
-          {email}
-        </Text>
-        <Text maxW="100%" overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis">
-          {userRole}
-        </Text>
-      </Stack>
-    </Card>
+    <Tools
+      body={
+        <Dialog
+          trigger={({ open }) => (
+            <Button onClick={open} justifyContent="flex-start" variant="ghost" leftIcon={<Trash size={24} />}>
+              Remover
+            </Button>
+          )}
+          title='Remover usuário'
+          message='Tem certeza que deseja remover este usuário?'
+          onConfirm={() => onDelete && onDelete(id)}
+        />
+      }
+    >
+      <Card w="150px" p={4} gap={2}>
+        <Stack>
+          <Avatar name={name} alignSelf="center" />
+          <Heading size="sm" maxW="100%" overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis">
+            {name}
+          </Heading>
+        </Stack>
+        <Stack maxW="120px">
+          <Text maxW="100%" overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis">
+            {email}
+          </Text>
+          <Text maxW="100%" overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis">
+            {userRole}
+          </Text>
+        </Stack>
+      </Card>
+    </Tools>
   )
 }
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,19 +1,88 @@
-import { UserCard } from "../components/UserCard"
-import { useFetch } from "../hooks/useFetch"
-import { DisplayLayout } from "../layouts/DisplayLayout"
-import { IUser } from "../types/types"
+import { Button, useToast } from '@chakra-ui/react'
+import { UserCard } from '../components/UserCard'
+import { useFetch } from '../hooks/useFetch'
+import { DisplayLayout } from '../layouts/DisplayLayout'
+import { api } from '../services/api'
+import { IUser } from '../types/types'
+import { Modal } from '../components/Modal'
+import { Plus } from '@phosphor-icons/react'
+import { Form } from '../components/Form'
+import { ISignUp, signUpSchema } from '../schemas/schemas'
 
 export const UsersPage = () => {
+  const { data, refetch } = useFetch<IUser[]>({ url: '/users' })
 
-  const { data } = useFetch<IUser[]>({ url: '/users'})
+  const toast = useToast()
+
+  const deleteUser = async (id: number) => {
+    try {
+      await api.delete(`users/${id}`)
+      toast({ description: 'Usuário removido com sucesso', status: 'success' })
+      refetch()
+    } catch (error) {
+      toast({ description: 'Não foi possível remover o usuário', status: 'error' })
+    }
+  }
+
+  const createUser = async (data: ISignUp) => {
+    try {
+      await api.post('users', data)
+      toast({ description: 'Usuário criado com sucesso', status: 'success' })
+      refetch()
+    } catch (error) {
+      toast({ description: 'Não foi possível criar o usuário', status: 'error' })
+    }
+  }
 
   return (
-    <DisplayLayout title="Usuários">
-      {
-        data?.map(user => (
-          <UserCard key={user.id} {...user}/>
-        ))
+    <DisplayLayout
+      title="Usuários"
+      action={
+        <Modal
+          trigger={({ open }) => (
+            <Button colorScheme="purple" onClick={open} leftIcon={<Plus size={24} />}>
+              Criar Administrador
+            </Button>
+          )}
+          title="Criar Administrador"
+          body={({ close }) => (
+            <Form
+              onSubmit={async (data) => {
+                await createUser(data)
+                close()
+              }}
+              structure={{
+                schema: signUpSchema,
+                defaultValues: {
+                  name: '',
+                  email: '',
+                  password: '',
+                },
+                inputs: [
+                  {
+                    name: 'name',
+                    label: 'Nome',
+                  },
+                  {
+                    name: 'email',
+                    label: 'Email',
+                  },
+                  {
+                    name: 'password',
+                    label: 'Senha',
+                    type: 'password',
+                  },
+                ],
+              }}
+              footer={(
+                <Button colorScheme='purple' type='submit'>Criar Administrador</Button>
+              )}
+            />
+          )}
+        />
       }
+    >
+      {data?.map((user) => <UserCard key={user.id} onDelete={deleteUser} {...user} />)}
     </DisplayLayout>
   )
 }
